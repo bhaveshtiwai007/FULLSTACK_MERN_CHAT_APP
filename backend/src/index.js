f706bdb04673a50a85b711ab2cf0cc5fc884abfc
Bhavesh Tiwari <2211301165@stu.main.ac.in>
2025-06-23 10:33:05 +0530

    Real Time Chat App

--- backend/src/index.js
+++ backend/src/index.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 
-server.listen(5001, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
 });
